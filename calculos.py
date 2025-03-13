1724456eb699e7ad12ae32fabda1ea5c1817a260
breno762 <brenogithub608@gmail.com>
2025-03-12 21:43:48 -0300

    Add files via upload

---
+++ calculos.py
@@ -0,0 +1,51 @@
+1. preco = 150.0
+desconto = 20
+preco_final = preco - (preco * desconto / 100)
+print("Preço final após desconto:", preco_final)
+
+//
+
+
+2. peso = 70
+altura = 1.75
+print("Seu IMC é:", 70 / 1.75 ** 2)
+
+//
+
+3. idade = 22
+tem_experiencia = True
+elegivel = idade >= 18 and tem_experiencia
+print("Elegível para o concurso?", elegivel)
+
+//
+
+4. nivel_acesso = 2
+if nivel_acesso == 3:
+    print("Acesso total")
+elif nivel_acesso == 2:
+    print("Acesso parcial")
+else:
+    print("Acesso restrito")
+
+//
+
+5. celsius = 30
+fahrenheit = (celsius * 9/5) + 32
+print("Temperatura em Fahrenheit:", fahrenheit)
+
+//
+
+6. numero = 7
+if numero % 2 == 0:
+    print("O número é par.")
+else:
+    print("O número é ímpar.")
+
+//
+
+7.
+
+
+
+
+
