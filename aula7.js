b5da6cd34042bfe4c4c3d33ef131545180d659b0
breno762 <brenogithub608@gmail.com>
2025-03-17 18:09:13 -0300

    Add files via upload

---
+++ aula7.js
@@ -0,0 +1,19 @@
+let numero1 = parseInt(prompt("Digite um número para a contagem regressiva"))
+for (let i = numero1; i >= 0; i--) {
+    console.log(i);
+}
+
+let numero2 = parseInt(prompt("Digite um número"))
+let soma = 0;
+let indice = 1;
+while (indice <= numero2) {
+    soma += indice;
+    indice++;
+}
+console.log("Soma:", soma);
+
+let numero3 = prompt("Digite um número para ver sua tabuada")
+let numeroint = parseInt (numero3)
+for (let i = 1; <= 10; i++) {
+    console.log(*i={(numeroint) x ${i} = ${numeroint * i}*);
+}
